cf5daed477e8b66392173f4d95531ff76358b5a8
djtoohey <djtoohey@gmail.com>
2021-01-14 01:39:04 +1030

    processes data and outputs to textfield, with copybtn

--- script.js
+++ script.js
@@ -1,25 +1,101 @@
 // vars
-
 const name = document.getElementById("name");
 const sales = document.getElementById("sales");
+const salesTarget = document.getElementById("salesTarget");
 const labour = document.getElementById("labour");
+const labourTarget = document.getElementById("labourTarget");
 const customertx = document.getElementById("customertx");
+const customertxp = document.getElementById("customertxp");
 const cash = document.getElementById("cash");
 const wastage = document.getElementById("wastage");
 
-
 const submitBtn = document.getElementById("submit");
 
-
+const docField = document.getElementById("doc");
+const copyBtn = document.getElementById("copy");
 
 submitBtn.addEventListener("click", function () {
-    console.log("name", name.value);
-    console.log("sales", sales.value);
-    console.log("labour", labour.value);
-    console.log("customertx", customertx.value);
-    console.log("cash", cash.value);
-    console.log("wastage", wastage.value);
+    // Date
+    let date = new Date();
+    let dateStr = "";
+    dateStr = dateStr + date;
+    dateStr = dateStr.slice(0, 15);
+
+    // Sales
+    // Sales were $???, under target by $???
+    let salesTxt = "";
+    if (salesTarget.value > 0) {
+        salesTxt = `Sales were $${sales.value}, over target by $${salesTarget.value}`;
+    }
+    else if (salesTarget.value < 0) {
+        salesTxt = `Sales were $${sales.value}, under target by $${salesTarget.value}`;
+    }
+    else {
+        salesTxt = `Sales were $${sales.value}`;
+    }
+
+
+    // Labour
+    // Labour was $???, under target by $???
+    let labourTxt = "";
+    if (labourTarget.value > 0) {
+        labourTxt = `Labour was $${labour.value}, over target by $${labourTarget.value}`;
+    }
+    else if (labourTarget.value < 0) {
+        labourTxt = `Labour was $${labour.value}, under target by $${labourTarget.value}`;
+    }
+    else {
+        labourTxt = `Labour was $${labour.value}`;
+    }
+
+    // CustomerTX
+    // Customer Transactions were ???, ??% of expected
+    let customertxTxt = `Customer Transactions were ${customertx.value}, ${customertxp.value}% of expected`
+
+
+    // Cash
+    // Cash was +/-$???
+    let cashFormatted = 0;
+    if (cash.value > 0) {
+        cashFormatted = "+$" + cash.value;
+    }
+    else if (cash.value < 0) {
+        cashFormatted = "-$" + (cash.value * -1);
+    }
+    else {
+        cashFormatted = "$" + cash.value;
+    }
+
+    let cashTxt = `Cash was ${cashFormatted}`
+
+    // Wastage
+    // Wastage was only $???
+    let wastageTxt = `Wastage was only $${wastage.value}`
+
+    const doc = `Good morning!
+ 
+Quick summary of ${dateStr}:
+    
+${salesTxt}
+    
+${labourTxt}
+    
+${customertxTxt}
+    
+${cashTxt}
+    
+${wastageTxt}
+    
+Have a good day
+    
+${name.value}`;
+
+    docField.value = doc;
 
 
 });
 
+copyBtn.addEventListener("click", function () {
+    docField.select();
+    document.execCommand("copy");
+});
